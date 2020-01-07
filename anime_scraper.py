6cc754a7d2e0701afec208ed9325a036fd68f95a
Alvian Joseph <alvianjoseph@gmail.com>
2020-01-07 09:28:06 -0800

    adds some docstrings and tests

--- anime_scraper.py
+++ anime_scraper.py
@@ -6,6 +6,15 @@ pp = pprint.PrettyPrinter()
 import requests
 
 def append_page_number_to_url(url_dict, page_number=None):
+  '''
+  In a seperate file we have a dictionary with keys
+  in this format: https://www.crunchyroll.com/one-piece/reviews/helpful/page
+  This function appends a number at the end of the link for every
+  number in a given range. The links are then appended to a list
+  associated with the appropriate key.
+  This allows us to grab as many links as we need while only having to 
+  manually grab them once.
+  '''
   page_number = page_number or 6
 
   for link in url_dict:
@@ -14,29 +23,50 @@ def append_page_number_to_url(url_dict, page_number=None):
       url_dict[link]['url_list'].append(new_url)
   return url_dict
 
-def get_reviews(show_dict):
+def get_reviews(anime_dict):
+  '''
+  This function takes in a formatted dictionary
+  It iterates through the url lists generated in the
+  previous function and uses beautiful soup to scrape
+  the review data. Class names were found by inspecting 
+  the crunchy roll site using chrome's dev tools.
+  The reviews are appended to a list in the dictionary
+  and we return the modified dictionary.
+  '''
   
-  for key in show_dict:
-    for url in show_dict[key]['url_list']:
+  for key in anime_dict:
+    for url in anime_dict[key]['url_list']:
       markup = requests.get(url).text
       soup = BeautifulSoup(markup, 'html.parser')
       reviews = soup.find_all('div', {'class':'trunc'}, {'class': 'more'})
       for review in reviews:
-        show_dict[key]['reviews'].append(review.text)
+        anime_dict[key]['reviews'].append(review.text)
 
-  return show_dict
+  return anime_dict
 
-def get_ratings(show_dict):
-  for key in show_dict:
-    for url in show_dict[key]['url_list']:
+def get_ratings(anime_dict):
+  '''
+  This function takes in a formatted dictionary
+  Similar to the above function, it iterates
+  over the url list but instead grabs the ratings,
+  which should be a string number between 1 and 5
+  '''
+  for key in anime_dict:
+    for url in anime_dict[key]['url_list']:
       markup = requests.get(url).text
       soup = BeautifulSoup(markup, 'html.parser')
       ratings = soup.find_all(class_='rating-widget-static-large')
       for rating in ratings:
-        show_dict[key]['ratings'].append(rating['content'])
-  return show_dict
+        anime_dict[key]['ratings'].append(rating['content'])
+  return anime_dict
 
 def save_to_dictionary():
+  '''
+  This function calls all the helper functions and returns
+  a full dictionary with all lists filled out.
+  It's then saved to a csv file. Ideally you just run
+  this function one time to get all the data you need at once.
+  '''
   pages = append_page_number_to_url(shows_testing, 3)
   reviews = get_reviews(pages)
   ratings = get_ratings(reviews)
--- test_anime_scraper.py
+++ test_anime_scraper.py
@@ -1,4 +1,4 @@
-from anime_scraper import append_page_number_to_url
+from anime_scraper import append_page_number_to_url, get_ratings, get_reviews
 import pytest
 @pytest.fixture
 def my_list():
@@ -34,3 +34,9 @@ def test_append_page_number_with_page(my_list):
   expected_one_piece = ['https://www.crunchyroll.com/one-piece/reviews/helpful/page1', 'https://www.crunchyroll.com/one-piece/reviews/helpful/page2']
   assert my_list['https://www.crunchyroll.com/my-hero-academia/reviews/helpful/page']['url_list'] == expected_my_hero
   assert my_list['https://www.crunchyroll.com/one-piece/reviews/helpful/page']['url_list'] == expected_one_piece
+
+def test_get_reviews(my_list):
+  append_page_number_to_url(my_list, 2)
+  get_reviews(my_list)
+  assert len(my_list['https://www.crunchyroll.com/one-piece/reviews/helpful/page']['reviews']) == 10
+  assert len(my_list['https://www.crunchyroll.com/my-hero-academia/reviews/helpful/page']['reviews']) == 10
